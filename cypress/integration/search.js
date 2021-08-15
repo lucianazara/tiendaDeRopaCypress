1f3ae38e635650b4f154d30da5bc71642737813f
Lu Zara <lucininiz@gmail.com>
2021-08-15 14:49:26 -0300

    Pruebas

---
+++ cypress/integration/search.js
@@ -0,0 +1,30 @@
+
+
+
+describe('Search elements', () =>{
+    beforeEach(()=>{
+        cy.visit("http://automationpractice.com/index.php");
+    })
+    it('search for elements with multiple results', () =>{
+
+        cy.fixture("index").then((index)=>{
+            cy.get(index.searchBox).type("dress")
+            cy.get(index.searchButton).click();
+
+        })
+        cy.fixture('searchResult').then((searchResult)=>{
+            cy.get(searchResult.title).should('contain', 'dress');
+        })
+        })
+        it("search for elements with no results", ()=>{
+                cy.fixture("index").then((index)=>{
+                    cy.get(index.searchBox).type('qwerty');
+                    cy.get(index.searchButton).click();
+                })
+                cy.fixture('searchResult').then((searchResult)=>{
+                    cy.get(searchResult.alert).should("contain", "No results were found for your search");
+            
+                })
+            })
+        })
+    
